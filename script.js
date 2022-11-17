8f8399addcdcf0c56ab940fddfccfca5f849715e
Iskelo <iskelo.kap@gmail.com>
2022-11-17 16:46:37 +0100

    update srcipt

--- script.js
+++ script.js
@@ -2,13 +2,11 @@ const menu = document.querySelector('.header__menu');
 const burger = document.querySelector('.header__burger');
 const b = document.querySelector('body');
 
-
 burger.addEventListener('click', () => {
 	burger.classList.toggle('active');
 	menu.classList.toggle('active');
 	b.classList.toggle('lock');
 });
-
 
 document.querySelectorAll('.header__link').forEach(n => n.addEventListener("click", () => {
 	burger.classList.remove('active');
